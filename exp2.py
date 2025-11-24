5356fd52e35c7e73f68950b253e1c4f5a111d975
BhuvaaneshR <220701045@rajalakshmi.edu.in>
2025-11-24 09:08:23 +0530

    Exp2-1

--- exp2.py
+++ exp2.py
@@ -1,3 +1,4 @@
+#Build a Convolutional Neural Network with Keras/TensorFlow
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
